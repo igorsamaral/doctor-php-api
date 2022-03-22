d470c9d7747dfe204d00cc08bdac9602753ada71
Igor Santana <financeiro@squadx.dev>
2022-03-22 20:26:05 -0300

    feat: criado rota tipo resource para "doctors"

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -2,3 +2,7 @@
 
 use App\Http\Controllers\Api\DoctorController;
 use Pecee\SimpleRouter\SimpleRouter as Route;
+
+Route::group(['prefix' => '/api'], function() {
+    Route::resource('/doctors', DoctorController::class);
+});
